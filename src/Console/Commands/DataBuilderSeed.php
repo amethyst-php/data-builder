550cb9fd776fd97457f1906f4e07de8443ff5bf8
railken <railken@protonmail.ch>
2019-05-23 22:36:04 +0200

    fix style

--- src/Console/Commands/DataBuilderSeed.php
+++ src/Console/Commands/DataBuilderSeed.php
@@ -3,10 +3,10 @@
 namespace Railken\Amethyst\Console\Commands;
 
 use Illuminate\Console\Command;
+use Illuminate\Support\Arr;
+use Railken\Amethyst\DataBuilders\CommonDataBuilder;
 use Railken\Amethyst\Managers\DataBuilderManager;
 use Symfony\Component\Yaml\Yaml;
-use Railken\Amethyst\DataBuilders\CommonDataBuilder;
-use Illuminate\Support\Arr;
 
 class DataBuilderSeed extends Command
 {
@@ -31,41 +31,40 @@ class DataBuilderSeed extends Command
         });
 
         foreach ($managers as $classManager) {
-
             $dataBuilderRecord = $dataBuilderManager->updateOrCreateOrFail([
-                'name'        => (new $classManager())->getName().' by dates',
+                'name' => (new $classManager())->getName().' by dates',
             ], [
-                'class_name'  => CommonDataBuilder::class,
+                'class_name'      => CommonDataBuilder::class,
                 'class_arguments' => $classManager,
-                'description' => 'Retrieve data between dates',
-                'filter'      => 'created_at gte "{{ from|date("Y-m-d 00:00:00") }}" and created_at lte "{{ to|date("Y-m-d 23:59:59") }}"',
-                'input'       => Yaml::dump([
-                    'from'=> [
-                        'validation'=> 'date',
-                        'type'      => 'date',
+                'description'     => 'Retrieve data between dates',
+                'filter'          => 'created_at gte "{{ from|date("Y-m-d 00:00:00") }}" and created_at lte "{{ to|date("Y-m-d 23:59:59") }}"',
+                'input'           => Yaml::dump([
+                    'from' => [
+                        'validation' => 'date',
+                        'type'       => 'date',
                     ],
-                    'to'=> [
-                        'validation'=> 'date',
-                        'type'      => 'date',
+                    'to' => [
+                        'validation' => 'date',
+                        'type'       => 'date',
                     ],
                 ]),
                 'mock_data' => Yaml::dump([
-                    'from'=> '2018-09-01T22:00:00.000Z',
-                    'to'  => '2018-09-08T22:00:00.000Z',
+                    'from' => '2018-09-01T22:00:00.000Z',
+                    'to'   => '2018-09-08T22:00:00.000Z',
                 ]),
             ])->getResource();
 
             $dataBuilderRecord = $dataBuilderManager->updateOrCreateOrFail([
-                'name'        => (new $classManager())->getName().' by id',
+                'name' => (new $classManager())->getName().' by id',
             ], [
-                'class_name'  => CommonDataBuilder::class,
+                'class_name'      => CommonDataBuilder::class,
                 'class_arguments' => $classManager,
-                'description' => 'Retrieve data by id',
-                'filter'      => 'id eq "{{ id }}"',
-                'input'       => Yaml::dump([
-                    'id'=> [
-                        'validation'=> 'integer',
-                        'type'      => 'text',
+                'description'     => 'Retrieve data by id',
+                'filter'          => 'id eq "{{ id }}"',
+                'input'           => Yaml::dump([
+                    'id' => [
+                        'validation' => 'integer',
+                        'type'       => 'text',
                     ],
                 ]),
                 'mock_data' => Yaml::dump([
